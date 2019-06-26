561550e1911afb4b4fce23ce49fa0b7610e196dd
Lexi <lexa.shelby@gmail.com>
2019-06-25 21:31:32 -0400

    first commit

---
+++ Deliverable 1 - Currency Converter/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace Deliverable_1__Currency_Format_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat1 = true, repeat2 = true, repeat3 = true;
+            double firstNumber = 0, secondNumber = 0, thirdNumber = 0;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            while (repeat1 == true)
+            {
+                Console.WriteLine("Type a dollar amount.");
+                string firstNumberString = Console.ReadLine();
+                if (Double.TryParse(firstNumberString, out firstNumber))
+                {
+                    Console.WriteLine(firstNumber);
+                    repeat1 = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized input. Please try again.");
+                    repeat1 = true;
+                }
+            }
+            while (repeat2 == true)
+            {
+                Console.WriteLine("Type a second dollar amount.");
+                string secondNumberString = Console.ReadLine();
+                if (Double.TryParse(secondNumberString, out secondNumber))
+                {
+                    Console.WriteLine(secondNumber);
+                    repeat2 = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized input. Please try again.");
+                    repeat2 = true;
+                }
+            }
+            while (repeat3 == true)
+            {
+                Console.WriteLine("Type a third dollar amount.");
+                string thirdNumberString = Console.ReadLine();
+                if (Double.TryParse(thirdNumberString, out thirdNumber))
+                {
+                    Console.WriteLine(thirdNumber);
+                    repeat3 = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized input. Please try again.");
+                    repeat3 = true;
+                }
+            }
+
+            double totalNumber = (firstNumber + secondNumber + thirdNumber);
+            totalNumber = Math.Round(totalNumber, 2);
+
+            var average = Math.Round((totalNumber / 3), 2);
+            var smallest = Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+            var largest = Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+            Console.WriteLine("The total number:" + totalNumber);
+            Console.WriteLine("The average number:" + average);
+            Console.WriteLine("The smallest number:" + smallest);
+            Console.WriteLine("The largest number:" + largest);
+
+            var usDollar = totalNumber;
+            var swedKrona = (totalNumber * 9.35);
+            var japYen = Math.Round((totalNumber * 107.33), 0);
+            var thaiBaht = (totalNumber * 30.82);
+
+            CultureInfo us = new CultureInfo("en-US");
+            CultureInfo swed = new CultureInfo("sv-SE");
+            CultureInfo jap = new CultureInfo("ja-JP");
+            CultureInfo thai = new CultureInfo("th-TH");
+
+            Console.WriteLine("The amount in US Dollars: " + (us.NumberFormat.CurrencySymbol) + usDollar);
+            Console.WriteLine("The amount in Swedish Krona: " + (swed.NumberFormat.CurrencySymbol) + swedKrona);
+            Console.WriteLine("The amount in Japanese Yen: " + (jap.NumberFormat.CurrencySymbol) + japYen);
+            Console.WriteLine("The amount in Thai Baht: " + (thai.NumberFormat.CurrencySymbol) + thaiBaht);
+            Console.ReadKey();
+        }
+    }
+}
+
